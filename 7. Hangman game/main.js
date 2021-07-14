dd75e2f80c70c6de15894485c592eebc06678157
Rafiur Rahman <rrfoundary@gmail.com>
2021-07-14 18:46:43 +0600

    7. hangman - Added show hidden word functionality

--- 7. Hangman game/main.js
+++ 7. Hangman game/main.js
@@ -9,3 +9,30 @@ const figureParts = document.querySelectorAll(".figure-part");
 const words = ["Applications", "Programming", "interface", "wizard"];
 
 let selectedWords = words[Math.floor(Math.random() * words.length)];
+
+const correctLetters = ["w", "i", "z", "a", "r", "d"];
+const wrongLetters = [];
+
+//show hidden word
+function displayWord() {
+  wordEl.innerHTML = `
+        ${selectedWords
+          .split("")
+          .map(
+            (letter) => `
+            <span class = "letter">
+                ${correctLetters.includes(letter) ? letter : ""}
+            </span>
+            `
+          )
+          .join("")}
+    `;
+
+  const innerWord = wordEl.innerText.replace(/\n/, "");
+  if (innerWord === selectedWords) {
+    finalMessage.innerText = "Congratulation ! You have won";
+    popup.style.display = "flex";
+  }
+  console.log(innerWord);
+}
+displayWord();
