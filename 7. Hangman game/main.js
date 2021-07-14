cc7eb6e596719a01686a51f9938b05aedbd8b2ad
Rafiur Rahman <rrfoundary@gmail.com>
2021-07-14 10:54:38 +0600

    7.hangman - Declare variables

---
+++ 7. Hangman game/main.js
@@ -0,0 +1,11 @@
+const wordEl = document.querySelector("#word");
+const wrongLetterEl = document.querySelector("#wrong-letters");
+const playAgainButton = document.querySelector("#play-button");
+const popup = document.querySelector("#popup-container");
+const notification = document.querySelector("#notification-container");
+const finalMessage = document.querySelector("#final-message");
+const figureParts = document.querySelectorAll(".figure-part");
+
+const words = ["Applications", "Programming", "interface", "wizard"];
+
+let selectedWords = words[Math.floor(Math.random() * words.length)];
