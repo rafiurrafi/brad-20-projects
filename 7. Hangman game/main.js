21daee6452b51d5583a3aedb92a17a0e27060841
Rafiur Rahman <rrfoundary@gmail.com>
2021-07-18 22:59:23 +0600

    Added show hidden word function

--- 7. Hangman game/main.js
+++ 7. Hangman game/main.js
@@ -6,3 +6,8 @@ const notification = document.querySelector("#notification-container");
 const finalMessage = document.querySelector("#final-message");
 const figureParts = document.querySelectorAll(".figure-part");
 
+const words = ["application", "programming", "interface", "wizard"];
+const selectedWord = words[Math.floor(Math.random() * words.length)];
+
+const correctLetters = [];
+const wrongLetters = [];
